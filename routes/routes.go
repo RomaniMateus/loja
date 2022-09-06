a7a3dad63717a8414e1102faf957a79ed36e1bf7
Mateus Romani <mateusromani@gmail.com>
2022-09-06 17:26:40 -0300

    transportando projeto do PC para o Github

---
+++ routes/routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/mateus/controllers"
+)
+
+func CarregaRotas() {
+	http.HandleFunc("/", controllers.Index)
+	http.HandleFunc("/new", controllers.New)
+	http.HandleFunc("/insert", controllers.Insert)
+	http.HandleFunc("/delete", controllers.Delete)
+	http.HandleFunc("/edit", controllers.Edit)
+}
