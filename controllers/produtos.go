b77f1d5179731ee6f9f12ca534f6c2eb6c9587e9
Mateus Romani <mateusromani@gmail.com>
2022-09-08 11:13:40 -0300

    Atualização dos produtos

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -54,3 +54,32 @@ func Edit(w http.ResponseWriter, r *http.Request) {
 	produto := models.EditaProduto(idDoProduto)
 	temp.ExecuteTemplate(w, "Edit", produto)
 }
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		idConvertida, err := strconv.Atoi(id)
+		if err != nil {
+			log.Println("Erro na conversão do ID para int:", err)
+		}
+
+		precoConvertido, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço para float:", err)
+		}
+
+		quantidadeConvertida, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão da quantidade:", err)
+		}
+
+		models.AtualizaProduto(idConvertida, nome, descricao, precoConvertido, quantidadeConvertida)
+	}
+
+	http.Redirect(w, r, "/", 301)
+}
